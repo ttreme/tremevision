6c68c86ec1da7f7b8144381201309cea2ca755a7
LucyMaude <lucymaude@Lucys-MacBook-Air.local>
2017-07-02 12:48:09 -0600

    connected service and search bar

--- service.js
+++ service.js
@@ -1,6 +1,11 @@
 var app = angular.module("myApp");
 
-app.service("allServices", ["$scope", "$http", function($scope, $http){
-    
-    $http.get("")
+app.service("allServices", ["$scope", "$http", function ($scope, $http) {
+
+    $scope.get = function (input) {
+
+        $http.get("").then(function (response) {
+            
+        });
+    }
 }])
