110096e741fb5b3c0fc2904ca590f4b02aa79cf4
Titus Treme <tremetitus@gmail.com>
2017-07-02 12:50:45 -0600

    changes

--- service.js
+++ service.js
@@ -1,5 +1,7 @@
 var app = angular.module("myApp");
 
+var API_KEY = 'AIzaSyC0vIdjWOXNdyXG--z_MGgz1YHU-9LIx_c';
+
 app.service("allServices", ["$scope", "$http", function($scope, $http){
     
     $http.get("")
